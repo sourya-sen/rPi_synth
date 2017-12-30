02584c4f5fe471514502ee01a66d6fbd812b2f82
Sourya Sen <ayruos@Souryas-MacBook-Pro.local>
2017-12-30 18:38:29 +0200

    ofAppNoWindow

--- rPi_Framework/src/main.cpp
+++ rPi_Framework/src/main.cpp
@@ -1,13 +1,12 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofAppNoWindow.h"
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	ofAppNoWindow window;
+	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+	//ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 	ofRunApp(new ofApp());
 
 }
