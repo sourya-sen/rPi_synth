2dbc9b0dcad365312995a238bf905efa2319cefe
Sourya Sen <ayruos@gmail.com>
2018-03-07 14:43:16 +0200

    fix windowMode typo

--- rPi_videoSynth/src/main.cpp
+++ rPi_videoSynth/src/main.cpp
@@ -5,7 +5,7 @@ int main( ){
 #ifdef TARGET_OPENGLES
     ofGLESWindowSettings settings;
     settings.glesVersion = 2;
-    settings.ofWindowMode = OF_FULLSCREEN;
+    settings.windowMode = OF_FULLSCREEN;
 #else
     ofGLWindowSettings settings;
     settings.setGLVersion(3,3);
